b4ba9c590d92e440f104a097b75252da56681c35
David Jardón <djardon@full-on-net.com>
2019-09-20 16:33:26 +0200

    Project created

---
+++ CursoiOS/CursoiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CursoiOS
+//
+//  Created by David Jardon on 19/09/2019.
+//  Copyright © 2019 ds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
